909eccbb395428f1bc0074daa621fc81e0a7ae69
min-log <minlog.0430@gmail.com>
2022-12-07 18:55:54 +0900

    slide

---
+++ resource/js/com/main.js
@@ -0,0 +1,20 @@
+
+$(function(){
+
+	$('.main_banner_img').slick({
+		slide: 'li',
+		infinite : false,
+		variableWidth:true,
+		initialSlide :0,
+		slidesToShow : 1,
+		slidesToScroll : 1,
+		arrows : false, 
+		autoplay : false,
+		dots: true
+	
+	});
+	
+
+
+
+});
