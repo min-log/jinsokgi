dc057853fa1b21521a1ffba6f0726fe911efabfa
min-log <minlog.0430@gmail.com>
2022-12-07 18:24:52 +0900

    header

---
+++ resource/js/com/main.js
@@ -0,0 +1,10 @@
+
+ $(".main_banner_img").slick({  
+       infinite: true , 
+       slidesToShow: 1, 
+       slidesToScroll: 1,
+       autoplay: false, 
+       //arrows:true,
+       dots:true, 
+       speed:1000 
+  });
