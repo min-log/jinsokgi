0cfd9a0a69e9dbc49ba705be3a3373a25ce21abb
min-log <minlog.0430@gmail.com>
2022-12-10 00:11:11 +0900

    d

--- resource/js/com/common.js
+++ resource/js/com/common.js
@@ -34,11 +34,11 @@ $(function(){
 	//scroll event
 	$(document).scroll(function () {
 		const scrollPoint = $(window).scrollTop(); // 현제 스크롤 위치 
-		const scrollPointH = $(window).scrollTop() + WindowH; // 현제 스크롤 위치 // 현제 스크롤 높이 + 전체 컨텐츠
+		const scrollPointH = $(window).scrollTop() - WindowH; // 현제 스크롤 위치 // 현제 스크롤 높이 + 전체 컨텐츠
 		const constrol = 40;
 
 		//메인 페이지 quick bar
-		console.log(footerT + '/' + scrollPointH)
+		console.log(footerT + '/' + scrollPoint)
 		
 		if (scrollPoint > footerT) {
 			QuickBar.removeClass('fixed');
