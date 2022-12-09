641e9e319cd31747a1650151079137fd4459d600
min-log <minlog.0430@gmail.com>
2022-12-10 00:23:25 +0900

    footer

--- resource/js/com/common.js
+++ resource/js/com/common.js
@@ -29,18 +29,18 @@ $(function(){
 	const WindowH = $(window).innerHeight(); // 윈도우 높이
 	const QuickBar = $('#quickBar');
 	const QuickBarH = QuickBar.innerHeight(); // 퀵 높이
-	const footerT = $('#footer').offset().top; //푸터 컨텐츠 높이
+	
 
 	//scroll event
 	$(document).scroll(function () {
 		const scrollPoint = $(window).scrollTop(); // 현제 스크롤 위치 
 		const scrollPointH = $(window).scrollTop() + WindowH; // 현제 스크롤 위치 // 현제 스크롤 높이 + 전체 컨텐츠
 		const constrol = 40;
-		const contentOffB = $('#container').offset().bottom;
+		const footerT = $('#footer').offset().top; //푸터 컨텐츠 높이
 
 		//메인 페이지 quick bar
 		console.log(footerT + '/' + scrollPoint)
-		console.log(contentOffB)
+		
 		if (scrollPoint >= footerT) {
 			QuickBar.addClass('fixed');
 		}else{
